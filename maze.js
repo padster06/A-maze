f55da73c941e5d6fc3749d8f807e97e71f6efbe9
padster06 <paddy2006@hotmail.co.uk>
2021-11-28 23:38:12 +0000

    enitial code I've have already written

---
+++ maze.js
@@ -0,0 +1,66 @@
+function removeWalls(current, next) {
+    if (next.i > current.i) {
+        current.properties.walls[1] = false;
+        next.properties.walls[0] = false;
+    }
+    if (next.i < current.i) {
+        current.properties.walls[0] = false;
+        next.properties.walls[1] = false;
+    }
+    if (next.j > current.j) {
+        current.properties.walls[3] = false;
+        next.properties.walls[2] = false;
+    }
+    if (next.j < current.j) {
+        current.properties.walls[2] = false;
+        next.properties.walls[3] = false;
+    }
+}
+
+function maze(current, prev) {
+    // let current = nodes[0][0];
+
+    current.properties.visisted = true;
+
+    let next = current.checkNeighbours(nodes);
+    // console.log(next);
+    current.properties.current = false;
+    let previous;
+    if (next) {
+        next.properties.visisted = true;
+
+        stack.push(current);
+
+        removeWalls(current, next);
+        current = next;
+    } else if (stack.length > 0) {
+        var node = stack.pop();
+        current = node;
+    } else {
+        endNode = nodes[nodes.length - 2][nodes[0].length - 1];
+
+        currNode = startNode;
+        currNodeIndex.i = i;
+        currNodeIndex.j = j;
+        startNode.dist = 0;
+        startNode.properties.start = true;
+        endNode.properties.end = true;
+        // console.log(nodes);
+        setInterval(loop, 30 / 1000);
+        return;
+    }
+    current.properties.current = true;
+
+    display.fillStyle = "#fff";
+    display.fillRect(0, 0, canvas.width, canvas.height);
+    let finished = true;
+    for (var i = 0; i < nodes.length; i++) {
+        for (var j = 0; j < nodes[i].length; j++) {
+            nodes[i][j].draw(display, pathFound);
+        }
+    }
+
+    setTimeout(() => {
+        maze(current, previous);
+    }, 0);
+}
