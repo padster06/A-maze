f1b85114b7c12fef481f891d8880f2ac9849d744
padster06 <paddy2006@hotmail.co.uk>
2021-12-04 16:13:56 +0000

    added percent counter and scalled up each node

--- main.js
+++ main.js
@@ -1,120 +1,120 @@
 var canvas, display;
 var stack = [];
 var nodes = [[]];
-var pathFound = false;
-var scale = 15;
+var pathFound = 20;
+var scale = 20;
 var currNode,
-    currNodeIndex = { i: 0, j: 0 };
+   currNodeIndex = { i: 0, j: 0 };
 var clicked = false;
 var mouse = {
-    x: 0,
-    y: 0,
+   x: 0,
+   y: 0,
 };
 
 function getIndexOfK(arr, k) {
-    for (var i = 0; i < arr.length; i++) {
-        var index = arr[i].indexOf(k);
-        if (index > -1) {
-            return [i, index];
-        }
-    }
+   for (var i = 0; i < arr.length; i++) {
+      var index = arr[i].indexOf(k);
+      if (index > -1) {
+         return [i, index];
+      }
+   }
 }
 
 function getNextNode(arr) {
-    var smallest = Number.MAX_SAFE_INTEGER - 1;
-    var curri, currj;
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[i].length; j++) {
-            if (
-                arr[i][j].dist + arr[i][j].heu < smallest &&
-                arr[i][j].properties.closed == false
-            ) {
-                smallest = arr[i][j].dist + arr[i][j].heu;
-                curri = i;
-                currj = j;
-            } else {
-                // console.log(arr[i][j].dist, arr[i][j].closed);
-            }
-        }
-    }
-    return arr[curri][currj];
+   var smallest = Number.MAX_SAFE_INTEGER - 1;
+   var curri, currj;
+   for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr[i].length; j++) {
+         if (
+            arr[i][j].dist + arr[i][j].heu < smallest &&
+            arr[i][j].properties.closed == false
+         ) {
+            smallest = arr[i][j].dist + arr[i][j].heu;
+            curri = i;
+            currj = j;
+         } else {
+            // console.log(arr[i][j].dist, arr[i][j].closed);
+         }
+      }
+   }
+   return arr[curri][currj];
 
-    // var smallest = Number.MAX_SAFE_INTEGER - 1;
-    // var curri, currj;
-    // for (var i = 0; i < arr.length; i++) {
-    //     for (var j = 0; j < arr[i].length; j++) {
-    //         if (
-    //             arr[i][j].dist < smallest &&
-    //             arr[i][j].properties.closed == false
-    //         ) {
-    //             smallest = arr[i][j].dist;
-    //             curri = i;
-    //             currj = j;
-    //         } else {
-    //             // console.log(arr[i][j].dist, arr[i][j].closed);
-    //         }
-    //     }
-    // }
-    // return arr[curri][currj];
+   // var smallest = Number.MAX_SAFE_INTEGER - 1;
+   // var curri, currj;
+   // for (var i = 0; i < arr.length; i++) {
+   //     for (var j = 0; j < arr[i].length; j++) {
+   //         if (
+   //             arr[i][j].dist < smallest &&
+   //             arr[i][j].properties.closed == false
+   //         ) {
+   //             smallest = arr[i][j].dist;
+   //             curri = i;
+   //             currj = j;
+   //         } else {
+   //             // console.log(arr[i][j].dist, arr[i][j].closed);
+   //         }
+   //     }
+   // }
+   // return arr[curri][currj];
 }
 
 function init() {
-    canvas = document.getElementById("canvas");
-    canvas.width = canvas.height = 70 * scale;
-    display = canvas.getContext("2d");
-    display.fillStyle = "#fff";
-    display.fillRect(0, 0, canvas.width, canvas.height);
-    canvas.addEventListener("mousemove", (e) => {
-        mouse.x = e.x;
-        // console.log(mouse.x);
-        mouse.y = e.y;
-        // mouse.sx = e.screenX;
-        // mouse
-        // console.log(mouse);
-    });
-    begin();
+   canvas = document.getElementById('canvas');
+   canvas.width = canvas.height = 50 * scale;
+   display = canvas.getContext('2d');
+   display.fillStyle = '#fff';
+   display.fillRect(0, 0, canvas.width, canvas.height);
+   canvas.addEventListener('mousemove', (e) => {
+      mouse.x = e.x;
+      // console.log(mouse.x);
+      mouse.y = e.y;
+      // mouse.sx = e.screenX;
+      // mouse
+      // console.log(mouse);
+   });
+   begin();
 }
 
 function preset() {
-    var i = Math.floor(Math.random() * (nodes.length - 1));
-    var j = Math.floor(Math.random() * (nodes[0].length - 1));
-    startNode = nodes[i][j];
-    endNode =
-        nodes[Math.floor(Math.random() * nodes.length)][
-            Math.floor(Math.random() * nodes[0].length)
-        ];
-    // for (var i = 0; i < 20; i++) {
-    //     nodes[i][nodes[0].length / 2].properties.wall = true;
-    // }
-    for (var i = 0; i < 50; i++) {
-        var tempi = Math.floor(Math.random() * (nodes.length - 5));
-        var tempj = Math.floor(Math.random() * (nodes[0].length - 5));
-        nodes[tempi][tempj].properties.wall = true;
-    }
+   var i = Math.floor(Math.random() * (nodes.length - 1));
+   var j = Math.floor(Math.random() * (nodes[0].length - 1));
+   startNode = nodes[i][j];
+   endNode =
+      nodes[Math.floor(Math.random() * nodes.length)][
+         Math.floor(Math.random() * nodes[0].length)
+      ];
+   // for (var i = 0; i < 20; i++) {
+   //     nodes[i][nodes[0].length / 2].properties.wall = true;
+   // }
+   for (var i = 0; i < 50; i++) {
+      var tempi = Math.floor(Math.random() * (nodes.length - 5));
+      var tempj = Math.floor(Math.random() * (nodes[0].length - 5));
+      nodes[tempi][tempj].properties.wall = true;
+   }
 }
 
 function begin() {
-    var setStart = (setEnd = false);
-    for (var i = 0; i < canvas.width / scale; i++) {
-        nodes.push(new Array());
-        for (var j = 0; j < canvas.height / scale; j++) {
-            nodes[i].push(new Node(i, j, scale));
-        }
-    }
+   var setStart = (setEnd = false);
+   for (var i = 0; i < canvas.width / scale; i++) {
+      nodes.push(new Array());
+      for (var j = 0; j < canvas.height / scale; j++) {
+         nodes[i].push(new Node(i, j, scale));
+      }
+   }
 
-    for (var i = 0; i < nodes.length; i++) {
-        for (var j = 0; j < nodes[i].length; j++) {
-            nodes[i][j].draw(display, pathFound);
-        }
-    }
-    startNode = nodes[0][0];
-    maze(nodes[0][0]);
-    // console.log(endNode);
+   for (var i = 0; i < nodes.length; i++) {
+      for (var j = 0; j < nodes[i].length; j++) {
+         nodes[i][j].draw(display, pathFound);
+      }
+   }
+   startNode = nodes[0][0];
+   maze(nodes[0][0], 1);
+   // console.log(endNode);
 
-    // document.getElementById("go").addEventListener("click", (e) => {
-    //     if (!clicked) {
-    //         clicked = true;
+   // document.getElementById("go").addEventListener("click", (e) => {
+   //     if (!clicked) {
+   //         clicked = true;
 
-    //     }
-    // });
+   //     }
+   // });
 }
--- maze.js
+++ maze.js
@@ -1,66 +1,74 @@
 function removeWalls(current, next) {
-    if (next.i > current.i) {
-        current.properties.walls[1] = false;
-        next.properties.walls[0] = false;
-    }
-    if (next.i < current.i) {
-        current.properties.walls[0] = false;
-        next.properties.walls[1] = false;
-    }
-    if (next.j > current.j) {
-        current.properties.walls[3] = false;
-        next.properties.walls[2] = false;
-    }
-    if (next.j < current.j) {
-        current.properties.walls[2] = false;
-        next.properties.walls[3] = false;
-    }
+   if (next.i > current.i) {
+      current.properties.walls[1] = false;
+      next.properties.walls[0] = false;
+   }
+   if (next.i < current.i) {
+      current.properties.walls[0] = false;
+      next.properties.walls[1] = false;
+   }
+   if (next.j > current.j) {
+      current.properties.walls[3] = false;
+      next.properties.walls[2] = false;
+   }
+   if (next.j < current.j) {
+      current.properties.walls[2] = false;
+      next.properties.walls[3] = false;
+   }
 }
 
-function maze(current, prev) {
-    // let current = nodes[0][0];
+function maze(current, total) {
+   // let current = nodes[0][0];
 
-    current.properties.visisted = true;
+   current.properties.visisted = true;
 
-    let next = current.checkNeighbours(nodes);
-    // console.log(next);
-    current.properties.current = false;
-    let previous;
-    if (next) {
-        next.properties.visisted = true;
+   document.getElementById('fract').innerHTML = `${total}/${
+      (canvas.width / scale) ** 2
+   }`;
 
-        stack.push(current);
+   document.getElementById('perc').innerHTML = `${
+      Math.floor((total / (canvas.width / scale) ** 2) * 100 * 100) / 100
+   }%`;
 
-        removeWalls(current, next);
-        current = next;
-    } else if (stack.length > 0) {
-        var node = stack.pop();
-        current = node;
-    } else {
-        endNode = nodes[nodes.length - 2][nodes[0].length - 1];
+   let next = current.checkNeighbours(nodes);
+   // console.log(next);
+   current.properties.current = false;
 
-        currNode = startNode;
-        currNodeIndex.i = i;
-        currNodeIndex.j = j;
-        startNode.dist = 0;
-        startNode.properties.start = true;
-        endNode.properties.end = true;
-        // console.log(nodes);
-        setInterval(loop, 30 / 1000);
-        return;
-    }
-    current.properties.current = true;
+   if (next) {
+      next.properties.visisted = true;
+      total++;
+      stack.push(current);
 
-    display.fillStyle = "#fff";
-    display.fillRect(0, 0, canvas.width, canvas.height);
-    let finished = true;
-    for (var i = 0; i < nodes.length; i++) {
-        for (var j = 0; j < nodes[i].length; j++) {
-            nodes[i][j].draw(display, pathFound);
-        }
-    }
+      removeWalls(current, next);
+      current = next;
+   } else if (stack.length > 0) {
+      var node = stack.pop();
+      current = node;
+   } else {
+      endNode = nodes[nodes.length - 2][nodes[0].length - 1];
 
-    setTimeout(() => {
-        maze(current, previous);
-    }, 0);
+      currNode = startNode;
+      currNodeIndex.i = i;
+      currNodeIndex.j = j;
+      startNode.dist = 0;
+      startNode.properties.start = true;
+      endNode.properties.end = true;
+      // console.log(nodes);
+      setInterval(loop, 30 / 1000);
+      return;
+   }
+   current.properties.current = true;
+
+   display.fillStyle = '#fff';
+   display.fillRect(0, 0, canvas.width, canvas.height);
+   let finished = true;
+   for (var i = 0; i < nodes.length; i++) {
+      for (var j = 0; j < nodes[i].length; j++) {
+         nodes[i][j].draw(display, pathFound);
+      }
+   }
+
+   setTimeout(() => {
+      maze(current, total);
+   }, 0);
 }
